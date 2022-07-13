19104e67ba9249667565ac2059075cc2aef0d2cc
Thania Beatriz <thania@tjro.jus.br>
2022-07-13 12:52:17 -0400

    att

---
+++ database/migrations/2022_07_13_164128_create_produto_detalhes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutoDetalhesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto_detalhes', function (Blueprint $table) {
+            $table->id();
+            $table->x('produto_id');
+            $table->float('comprimento', 8,2);
+            $table->float('largura', 8,2);
+            $table->float('altura', 8,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto_detalhes');
+    }
+}
